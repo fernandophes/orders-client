fac9c19ba12f3ea46e99b3682ebf106b7dff5db2
Fernando Silva <fernandophes@gmail.com>
2025-03-11 20:40:39 -0300

    Obtém o IP local como primeira configuração

--- src/main/java/br/edu/ufersa/cc/sd/utils/Constants.java
+++ src/main/java/br/edu/ufersa/cc/sd/utils/Constants.java
@@ -1,12 +1,23 @@
 package br.edu.ufersa.cc.sd.utils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class Constants {
 
-    public static final String DEFAULT_HOST = "localhost";
+    public static final String DEFAULT_HOST = getDefaultHost();
     public static final Integer LOCALIZATION_PORT = 8484;
+
+    private static final String getDefaultHost() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "localhost";
+        }
+    }
 
 }
